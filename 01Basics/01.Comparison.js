800fac17b9a391c1906618cf07f6b0c84520867f
RohitPaul11 <121225516+RohitPaul11@users.noreply.github.com>
2024-04-18 15:45:36 +0000

    Updated

--- 01Basics/01.Comparison.js
+++ 01Basics/01.Comparison.js
@@ -12,3 +12,9 @@ console.log(undefined==0);
 console.log(typeof null);   //Object
 
 console.log(typeof undefined);      //undefined
+
+
+//stack(Primitive)  , Heap(Non-Primitive)
+//Primitive values(num,bool) provide copy for the same variable and for Non-primitive(obj,array) we get 
+// direct references for the taking that value
+
