65cc16c35f4ac9ca9d75d1cd9f11f84eb1299bbf
RohitPaul11 <121225516+RohitPaul11@users.noreply.github.com>
2024-05-11 18:19:14 +0000

    If

---
+++ 02Controlflow/flow.js
@@ -0,0 +1,14 @@
+// if, if-else, else-if ladder, while ,do-while
+
+if(1==2){
+    console.log("Executed");
+}
+
+// >, <, ==, !==, ===, >=, <=
+const temp=25;
+if(temp==30){
+    console.log("Below 30");
+}
+else{
+    console.log("Above 30");
+}
